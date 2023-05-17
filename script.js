afdd8246c24e2598bb99e3e2acdf120f1aaf61fb
julianlosak <julianlosak@gmail.com>
2023-05-17 18:41:30 -0500

    Added random generator in jss and added an object to ustilize key value pairs

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const symbols = ["!", "@", "#"]
 const numbers = ["1", "2", "3"]
 const uppercaseLetters = ["A", "B", "C"]
 const lowercaseLetters = ["a", "b", "d"]
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
 
+  return randElement;
+}
 function generatePassword() {
   let passswordlength = prompt ("How long would you like the password")
   if (Number.isNaN(passswordlength)||passswordlength < 8||passswordlength > 128) {
@@ -18,6 +23,35 @@ function generatePassword() {
     alert("Must choose at least one option")
     return null
   }
+  let userOptions = {
+    passswordlength: passswordlength,
+    wantSymbols:  wantSymbols,
+    wantUppercaseLetters: wantUppercaseLetters,
+    wantLowercaseLetters: wantLowercaseLetters,
+    wantNumbers: wantNumbers,
+  }
+  console.log (userOptions)
+  let finalPassword = []
+  let potentialPassword = []
+  let chosenOptions = []
+  
+  if (userOptions.wantSymbols){
+  potentialPassword = potentialPassword.concat(symbols)
+  chosenOptions.push(getRandom(symbols))
+  }
+  if (userOptions.wantUppercaseLetters){
+    potentialPassword = potentialPassword.concat(uppercaseLetters)
+    chosenOptions.push(getRandom(uppercaseLetters))
+    }
+  if (userOptions.wantLowercaseLetters){
+      potentialPassword = potentialPassword.concat(lowercaseLetters)
+      chosenOptions.push(getRandom(lowercaseLetters))
+      }
+  if (userOptions.wantNumbers){
+        potentialPassword = potentialPassword.concat(numbers)
+        chosenOptions.push(getRandom(numbers))
+        }
+        console.log(potentialPassword, chosenOptions)
 }
 
 // Get references to the #generate element
